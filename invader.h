a6538c6a5f9fc8ff3317fdf3232b272c428ff7fe
ivansstyle <ivansstyle@me.com>
2015-11-25 18:56:11 +0000

    New update, missile, controls, movement and collision detection(still net working)

--- invader.h
+++ invader.h
@@ -1,16 +1,21 @@
 #ifndef INVADER_H__
 #define INVADER_H__
 
+//ENUMERATORS
 enum InvaderType{TYPE1,TYPE2,TYPE3};
-enum defenderBOOL{FALSE,TRUE};
+enum BOOL{FALSE,TRUE};
+enum InputControl{NONE,MOVE_LEFT,MOVE_RIGHT,SHOOT};
 
+//DEFENITIONS
 #define SPRITEWIDTH 30
 #define SPRITEHEIGHT 30
 #define COLS 11
 #define ROWS 5
 #define GAP 10
+#define DefenderSPEED 30
+#define MissileSPEED 5
 
-
+//Creating a new structure for the defender
 typedef struct
 {
   SDL_Rect pos;
@@ -19,15 +24,18 @@ typedef struct
   enum InvaderType type;
 }Invader;
 
+//Missile structure
+typedef struct{
+    SDL_Rect pos;
+    enum BOOL active;
+}TypeMissile;
 
 
-//Creating a new structure for the defender;
-enum InputControl{NONE,MOVE_LEFT,MOVE_RIGHT,SHOOT};
 
 typedef struct
 {
     SDL_Rect pos; //chech for correctness!
-    enum defenderBOOL alive; //if 0 = print game over on the screen
+    enum BOOL alive; //if 0 = print game over on the screen
     enum InputControl defender_event;
 
 }TypeDefender;
--- main.c
+++ main.c
@@ -12,27 +12,37 @@
 #define WIDTH 800
 // the height of the screen taking into account the maze and block
 #define HEIGHT 600
-// an enumeration for direction to move USE more enums!
 
-void PrintDebug(TypeDefender Defender);
+
+int test = 0;
+
+void PrintDebug(TypeDefender *Defender);
+void Controls(TypeDefender *Defender);
+int quit=0;
 
 void initializeDefender(TypeDefender *Defender);
-void updateDefender(TypeDefender *Defender);
+void initializeMissile(TypeMissile *Missile);
+void updateDefender(TypeDefender *Defender,TypeMissile *Missile);
 void drawDefender(SDL_Renderer *ren, SDL_Texture *tex,TypeDefender *Defender);
+void drawMissile(SDL_Renderer *ren, SDL_Texture *tex,TypeMissile *Missile);
 
 
 
 void initializeInvaders(Invader invaders[ROWS][COLS]);
 void updateInvaders(Invader invaders[ROWS][COLS]);
 void drawInvaders(SDL_Renderer *ren, SDL_Texture *tex,Invader invaders[ROWS][COLS]);
-
+void CollisionDetection(Invader invaders[ROWS][COLS], TypeMissile *Missile);
 int main()
 {
   Invader invaders[ROWS][COLS];
   TypeDefender Defender;
+  TypeMissile Missile;
+  printf("Programm Started.\n");
 
   initializeDefender(&Defender);
   initializeInvaders(invaders);
+  initializeMissile(&Missile);
+
   // initialise SDL and check that it worked otherwise exit
   // see here for details http://wiki.libsdl.org/CategoryAPI
   if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
@@ -83,55 +93,29 @@ int main()
 
 
 
-  int quit=0;
   // now we are going to loop forever, process the keys then draw
+
 
   while (quit !=1)
   {
-    SDL_Event event;
-    // grab the SDL even (this will be keys etc)
-    while (SDL_PollEvent(&event))
-    {
-      // look for the x of the window being clicked and exit
-      if (event.type == SDL_QUIT)
-        quit = 1;
-      // check for a key down
-      if (event.type == SDL_KEYDOWN)
-      {
-        switch (event.key.keysym.sym)
-        {
-        // if we have an escape quit
-        case SDLK_ESCAPE : quit=1; break;
-        }
-      }
-      if (event.type == SDLK_LEFT)
-      {
-         Defender.defender_event = MOVE_LEFT;
-      }
-      else if (event.type == SDLK_RIGHT)
-      {
-          Defender.defender_event = MOVE_RIGHT;
-      }
-      else if (event.type == SDLK_SPACE)
-      {
-          Defender.defender_event = SHOOT;
-      }
-     // else Defender.defender_event = NONE;
-  }
-    PrintDebug(Defender);
+     Controls(&Defender);
+     PrintDebug(&Defender);
 
-  // now we clear the screen (will use the clear colour set previously)
-  SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
-  SDL_RenderClear(ren);
-  //updateDefender(Defender);
+     // now we clear the screen (will use the clear colour set previously)
+     SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+     SDL_RenderClear(ren);
+
+  updateDefender(&Defender, &Missile);
   updateInvaders(invaders);
-  drawInvaders(ren,tex,invaders);
-  drawDefender(ren,tex,&Defender);
+  CollisionDetection(invaders, &Missile);
 
+    drawDefender(ren,tex,&Defender);
+    drawInvaders(ren,tex,invaders);
+    drawMissile(ren,tex,&Missile);
 
   // Up until now everything was drawn behind the scenes.
   // This will show the new, red contents of the window.
-  SDL_RenderPresent(ren);
+   SDL_RenderPresent(ren);
 
   }
 
@@ -147,26 +131,169 @@ void initializeDefender(TypeDefender *Defender)
   pos.x=WIDTH/2-30;
   pos.y=HEIGHT-50;
 
+  Defender->defender_event = NONE;
   Defender->alive = TRUE;
   Defender->pos = pos;
 
 }
-//Updating defender
-void updateDefender(TypeDefender *Defender)
+
+//Initializing Missile
+void initializeMissile(TypeMissile *Missile)
 {
+    SDL_Rect pos;
+    pos.w=SPRITEHEIGHT;
+    pos.h=SPRITEHEIGHT;
+    pos.x=WIDTH/2-30;
+    pos.y=HEIGHT-SPRITEHEIGHT-50;
+
+    Missile->active = FALSE;
+    Missile->pos = pos;
+}
+
+//Updating defender/Missile
+void updateDefender(TypeDefender *Defender, TypeMissile *Missile)
+{
+
     if (Defender->defender_event == MOVE_LEFT)
     {
-        if (Defender->pos.x > 0)
+        if (Defender->pos.x > DefenderSPEED)
         {
-            Defender->pos.x-=5;
+            Defender->pos.x-=DefenderSPEED;
         }
     }
     if (Defender->defender_event == MOVE_RIGHT)
     {
-        if (Defender->pos.x < WIDTH - SPRITEWIDTH)
+        if (Defender->pos.x < WIDTH - Defender->pos.w - DefenderSPEED)
         {
-            Defender->pos.x+=5;
+            Defender->pos.x+=DefenderSPEED;
         }
+    }
+
+
+    if (Defender->defender_event == SHOOT)
+    {
+        Missile->active = TRUE;
+    }
+
+    if (Missile->active == FALSE)
+    {
+        Missile->pos.x = Defender->pos.x + ((Defender->pos.w - Missile->pos.w) / 2);
+        Missile->pos.y = Defender->pos.y - Defender->pos.h;
+    }
+    if (Missile->active == TRUE)
+    {
+        if (Missile->pos.y> 0 - Missile->pos.h)
+        {
+            Missile->pos.y = Missile->pos.y - MissileSPEED;
+        }
+        else {Missile->active = FALSE;}
+
+       Defender->defender_event = NONE;
+       if (Missile->active == TRUE){
+           printf ("missile active\t");
+           printf ("Missile X:%d Missile Y:%d\n", Missile->pos.x, Missile->pos.y);
+       }
+    }
+    Defender->defender_event = NONE;
+}
+
+//Detecting Collision
+void CollisionDetection(Invader invaders[ROWS][COLS], TypeMissile *Missile)
+{
+       // A - invader B - missile //
+       // Adl - means A DOWN LEFT //
+    int A_dlX = 0; // A Down left
+    int A_drX = 0; // A Down right
+    int A_dlY = 0;
+    int A_ulY = 0;
+
+    int B_ulX = 0;
+    int B_urX = 0;
+    int B_ulY = 0;
+    int B_dlY = 0;
+
+    B_ulX = Missile->pos.x;
+    B_urX = Missile->pos.x + Missile->pos.w;
+    B_dlY = Missile->pos.y + Missile->pos.h;
+    B_ulY = Missile->pos.y;
+
+    if (Missile->active == TRUE)
+    {
+        printf("!!!invader X: %3d\n",invaders[3][9].pos.x);
+        printf("!!!invader Y: %3d\n",invaders[3][9].pos.y);
+        //int collision = 0;
+        for (int r=ROWS;r>=0; --r)
+        {
+
+            for (int c=COLS;c>=0; --c)
+            {
+                A_dlX = invaders[r][c].pos.x;
+                A_drX = invaders[r][c].pos.x + invaders[r][c].pos.w;
+                A_dlY = invaders[r][c].pos.y + invaders[r][c].pos.h;
+                A_ulY = invaders[r][c].pos.y;
+
+                if (B_ulX >= A_dlX)
+                {
+                    if (B_urX <= A_drX)
+                    {
+                        if (B_ulY >= A_dlY)
+                        {
+                            if (B_ulY <= A_ulY)
+                            {
+                                printf("invader X: %3d\n",invaders[r][c].pos.x);
+                                printf("invader Y: %3d\n",invaders[r][c].pos.y);
+
+
+                                //Collision
+                                printf("Collision!!!\n");
+                                printf("A_dlX = %3d A_drX = %3d A_dlY = %3d A_ulY = %3d \nB_ulX = %3d B_urX = %3d B_dlY = %3d B_ulY = %3d\n",
+                                        A_dlX, A_drX, A_dlY, A_ulY, B_ulX, B_urX, B_dlY, B_ulY);
+                            }
+                        }
+                    }
+                }
+
+
+//                if (invaders[r][c].pos.x <= Missile->pos.x)
+//                {
+//                    if (invaders[r][c].pos.x + invaders[r][c].pos.w >= Missile->pos.x)
+//                    {
+//                        if (invaders[r][c].pos.y >= Missile->pos.y)
+//                        {
+//                            if(invaders[r][c].pos.y + invaders[r][c].pos.h <= Missile->pos.y)
+//                            {
+//                                //Collision
+//                                collision = 1;
+//                                invaders[r][c].active = 0;
+//                            }
+//                        }
+//                    }
+//                }
+//                if (invaders[r][c].pos.x + invaders[r][c].pos.w >= Missile->pos.x + Missile->pos.w)
+//                {
+//                    if (invaders[r][c].pos.x <= Missile->pos.x + Missile->pos.w)
+//                    {
+//                        if (invaders[r][c].pos.y >= Missile->pos.y)
+//                        {
+//                            if(invaders[r][c].pos.y + invaders[r][c].pos.h <= Missile->pos.y)
+//                            {
+//                                //Collision
+//                                if (collision == 0)
+//                                    collision = 1;
+//                                if (invaders[r][c].active == 1)
+//                                    invaders[r][c].active = 0;
+//                            }
+//                        }
+//                    }
+//                }
+//            if (collision == 1)
+//                {
+//                    r = -1;
+//                Missile->active = FALSE;
+//                }
+            }
+        }
+
     }
 }
 
@@ -183,8 +310,18 @@ void drawDefender(SDL_Renderer *ren, SDL_Texture *tex,TypeDefender *Defender)
   SDL_RenderCopy(ren, tex,&SrcR, &Defender->pos);
 
 }
-
-
+//Drawing Missile
+void drawMissile(SDL_Renderer *ren, SDL_Texture *tex,TypeMissile *Missile)
+{
+  SDL_Rect SrcR;
+  SrcR.x=0;
+  SrcR.y=0;
+  SrcR.w=88;
+  SrcR.h=64;
+  SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+  SDL_RenderFillRect(ren,&Missile->pos);
+  SDL_RenderCopy(ren, tex,&SrcR, &Missile->pos);
+}
 
 void initializeInvaders(Invader invaders[ROWS][COLS])
 {
@@ -275,15 +412,52 @@ void updateInvaders(Invader invaders[ROWS][COLS])
   }
 }
 
-void PrintDebug(TypeDefender Defender)
+void PrintDebug(TypeDefender *Defender)
 {
-    if (Defender.defender_event == SHOOT){
-        printf("Defender: command shoot");
+    if (Defender->defender_event == SHOOT){
+        printf("Defender: command shoot\n");
     }
-    if (Defender.defender_event == MOVE_LEFT){
-        printf ("Defender: command left");
+    if (Defender->defender_event == MOVE_LEFT){
+        printf ("Defender: command left\n");
     }
-    if (Defender.defender_event == MOVE_RIGHT){
-        printf("Defender: command right");
+    if (Defender->defender_event == MOVE_RIGHT){
+        printf("Defender: command right\n");
+    }
+}
+
+//All SDL_Events now moved here,so not to interrupt with other calculations and for easy access.
+void Controls(TypeDefender *Defender)
+{
+    SDL_Event event;
+    // grab the SDL even (this will be keys etc)
+    while (SDL_PollEvent(&event))
+    {
+      // look for the x of the window being clicked and exit
+      if (event.type == SDL_QUIT)
+        quit = 1;
+      // check for a key down
+      if (event.type == SDL_KEYDOWN)
+      {
+        switch (event.key.keysym.sym)
+        {
+        // if we have an escape quit
+        case SDLK_ESCAPE : quit=1;
+            break;
+
+        case SDLK_LEFT : Defender->defender_event = MOVE_LEFT;
+            break;
+        case SDLK_RIGHT : Defender->defender_event = MOVE_RIGHT;
+            break;
+        case SDLK_SPACE : Defender->defender_event = SHOOT;
+            break;
+
+
+         // else Defender.defender_event = NONE;
+        }
+      }
+    }
+    if (test == 1){
+        Defender->defender_event = SHOOT;
+        test = 0;
     }
 }
